9531be0c598f3589069ec24d0fbebe2df34944ae
MikeData <Mikelovesbooks@gmail.com>
2016-10-25 09:33:40 +0100

    Removed some code thats not applicable here for readability

--- FDI_Dataset_recipe_9.py
+++ FDI_Dataset_recipe_9.py
@@ -24,23 +24,16 @@ def per_tab(tab):
     tab.dimension(PARAMS(0), PARAMS(1))    
     
     tab.excel_ref('A2').dimension('Type', CLOSEST, ABOVE)    
-    
-    unwanted = tab.excel_ref('E').is_not_blank().filter('2013.0').fill(LEFT)
-    unwanted = unwanted | tab.excel_ref('D').is_not_blank().filter('2013.0').fill(LEFT)
 
     # Get first and second part of location
     find = tab.excel_ref('A').is_not_blank().is_not_whitespace()
-    find = find - unwanted
-    # find = find | find.shift(1, 1)
     find.dimension("Area", CLOSEST, ABOVE)
     
     find = tab.excel_ref('B').is_not_blank().is_not_whitespace()
-    find = find - unwanted
     find = find | tab.excel_ref('A1').fill(DOWN).is_not_blank().shift(RIGHT)
     find.dimension("Area 1", CLOSEST, ABOVE)    
     
     find = tab.excel_ref('C').is_not_blank().is_not_whitespace()
-    find = find - unwanted
     find = find | tab.excel_ref('A1').fill(DOWN).is_not_blank().shift(2, 0)
     find.dimension("Area 2", CLOSEST, ABOVE) 
     
